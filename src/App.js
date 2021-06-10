38402124d269c893afea0d4ddfbe1e024997169a
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-06-10 17:39:29 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//import React from 'react';
+//import './index.css';
+//import App from './App';
+//import React from "react";
+//import"./App.css";
+//import"Weather-icons/css/Weather-icons.css";
+//import"bootstrap/dist/css/bootstrap.min.css";
+//import Weather from "./app_component/Weather.component";//
+
+//import logo from './logo.svg';//
+
+import React from "react";
+import"./App.css";
+import"Weather-icons/css/Weather-icons.css";
+import"bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./app_component/Weather.component";
+
+function App() {
+  return (
+    <div classname="App">
+    <Weather/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/app_component/weather.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const weather = () =>{
+    return(
+
+        <div className="container">
+            <div className="cards">
+                <h1>London</h1>
+                <h5 className="py-4">
+                <i className="wi wi-day-sunny display-5"/>
+                <h1 className="py-2"></h1>
+                </h5>
+            </div>
+        </div>
+    );
+};
+
+export default weather;
